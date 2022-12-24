d7d6de9c16648a6147e7527038fc0502c2f00fbd
Mykolas <mykolas.kucinskas2@gmail.com>
2022-12-24 14:17:57 +0200

    added configurations for ECR

---
+++ ecs-learning.tf
@@ -0,0 +1,24 @@
+terraform {
+    required_providers {
+      aws = {
+      source = "hashicorp/aws"
+      version = "4.48.0"
+    }
+  }
+  
+}
+
+# My main region is eu-north-1 (Stockholm).
+provider "aws" {
+  region = "eu-north-1"
+}
+
+resource "aws_ecr_repository" "employee_man_app_ecr_repository" {
+  name = "employee-management-app"
+  image_tag_mutability = "FALSE"
+
+  image_scanning_configuration {
+    scan_on_push = "false"
+  } 
+}
+
